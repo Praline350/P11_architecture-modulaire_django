691c6ccb109ffd36c0c37c44433ee7b13b7e2d1c
Edwin_Thierry <147498474+Praline350@users.noreply.github.com>
2024-10-05 12:37:17 +0200

    Refacto OK | Flake Ok

--- lettings/models.py
+++ lettings/models.py
@@ -2,20 +2,21 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinLengthValidator
 
 
-
 class Address(models.Model):
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
     street = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
     state = models.CharField(max_length=2, validators=[MinLengthValidator(2)])
     zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
-    country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
+    country_iso_code = models.CharField(
+        max_length=3, validators=[MinLengthValidator(3)]
+    )
 
     def __str__(self):
-        return f'{self.number} {self.street}'
-    
+        return f"{self.number} {self.street}"
+
     class Meta:
-        db_table = 'oc_lettings_site_address'
+        db_table = "oc_lettings_site_address"
 
 
 class Letting(models.Model):
@@ -24,6 +25,6 @@ class Letting(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     class Meta:
-        db_table = 'oc_lettings_site_letting'
+        db_table = "oc_lettings_site_letting"
