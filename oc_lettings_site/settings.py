4db66d5506f125f333133457d752e47305e3ddfa
Edwin_Thierry <147498474+Praline350@users.noreply.github.com>
2024-10-10 12:28:21 +0200

    Turn secret_key in env

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret
 SECRET_KEY = os.getenv("SECRET_KEY", default="fallback_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
