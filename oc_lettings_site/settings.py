906747279216113239feb0bdd67ff960d0d93ae1
Edwin_Thierry <147498474+Praline350@users.noreply.github.com>
2024-11-19 09:15:23 +0100

    Fix sentry Dsn

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -52,8 +52,7 @@ ROOT_URLCONF = "oc_lettings_site.urls"
 # Initialisation de la journalisation Sentry
 
 sentry_sdk.init(
-    dsn="""https://13719de764b893bd0b243922b51f5cf7
-    @o4507962541998080.ingest.de.sentry.io/4508121972670544""",
+    dsn=os.environ.get("SENTRY_DSN"),
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for tracing.
     traces_sample_rate=1.0,
